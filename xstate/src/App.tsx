64e857685c4e94a26addc0c8b4adf92da65fa710
zxf4399 <zxf4399@gmail.com>
2023-03-08 15:01:58 +0800

    test(xstate): actions sequence

--- xstate/src/App.tsx
+++ xstate/src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createMachine, interpret } from "xstate";
+
+const triggerMachine = createMachine(
+  {
+    id: "trigger",
+    initial: "inactive",
+    states: {
+      inactive: {
+        on: {
+          TRIGGER: {
+            target: "active",
+            // transition actions
+            actions: ["activate", "sendTelemetry"],
+          },
+        },
+      },
+      active: {
+        // entry actions
+        entry: ["notifyActive", "sendTelemetry"],
+        // exit actions
+        exit: ["notifyInactive", "sendTelemetry"],
+        on: {
+          STOP: { target: "inactive" },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      // action implementations
+      activate: (context, event) => {
+        console.log("activating...");
+      },
+      notifyActive: (context, event) => {
+        console.log("active!");
+      },
+      notifyInactive: (context, event) => {
+        console.log("inactive!");
+      },
+      sendTelemetry: (context, event) => {
+        console.log("time:", Date.now());
+      },
+    },
+  }
+);
+
+const triggerService = interpret(triggerMachine).onTransition((state) =>
+  console.log(state.history?.value, state.value)
+);
+
+triggerService.start();
+
+triggerService.send("TRIGGER");
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return null;
 }
 
-export default App
+export default App;
